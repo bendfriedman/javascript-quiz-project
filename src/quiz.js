dfa74db0812c417dc5f503b91f4b85934628eefa
Alexandra Busson <alexandra_b@live.fr>
2024-02-20 18:15:40 +0100

    day2 complited

--- src/quiz.js
+++ src/quiz.js
@@ -1,8 +1,8 @@
 class Quiz {
   constructor(questions, timeLimit, timeRemaining) {
-    this.questions = questions;
-    this.timeLimit = timeLimit;
-    this.timeRemaining = timeRemaining;
+    this.questions = questions; //array
+    this.timeLimit = timeLimit; //number
+    this.timeRemaining = timeRemaining; // number
     this.correctAnswers = 0;
     this.currentQuestionIndex = 0;
   }
@@ -37,4 +37,23 @@ class Quiz {
       return false;
     }
   }
+
+  filterQuestionsByDifficulty(difficulty) {
+    if (difficulty >= 1 && difficulty <= 3) {
+
+      const tempQuestionsArray = this.questions.filter((currentQuestion)=> currentQuestion.difficulty === difficulty);
+      
+      this.questions = tempQuestionsArray;
+      //this.questions.splice(0, this.questions.length, ...tempQuestionsArray);
+    }
+  }
+
+  averageDifficulty() {
+    let sumOfDifficulty = this.questions.reduce((acc, currentQuestion) => acc + currentQuestion.difficulty, 0)
+    
+    return Math.round(sumOfDifficulty / this.questions.length)
+  }
 }
+
+
+
